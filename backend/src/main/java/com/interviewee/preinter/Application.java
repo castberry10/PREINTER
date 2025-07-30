498fc687756c944b8e36419a8615db4d25fb25cd
Aoaoaoqq <tlfgj8@gmail.com>
2025-07-30 20:00:07 +0900

    test2

--- backend/src/main/java/com/interviewee/preinter/Application.java
+++ backend/src/main/java/com/interviewee/preinter/Application.java
@@ -2,8 +2,17 @@ package com.interviewee.preinter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				DataSourceAutoConfiguration.class,
+				DataSourceTransactionManagerAutoConfiguration.class,
+				HibernateJpaAutoConfiguration.class
+		}
+)
 public class Application {
 
 	public static void main(String[] args) {
