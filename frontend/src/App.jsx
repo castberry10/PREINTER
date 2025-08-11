b5a3ba6f0d5564a0fe20b60b6b538973412e52d2
castberry <sable@kangwon.ac.kr>
2025-08-11 21:28:29 +0900

    Update API base URL in App component; add 'aoaoaoqq.com' to allowedHosts in Vite server configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,8 @@ import LoginPage from './pages/LoginPage';
 import axios from 'axios';
 
 function App() {
-  axios.defaults.baseURL = "http://54.180.25.169:8080/api";
+  axios.defaults.baseURL = "http://preinter.castberry.kr:8443/api";
+  // axios.defaults.baseURL = "https://aoaoaoqq.com:8443/api";
   // axios.defaults.withCredentials = true;
   return (
     <div>
